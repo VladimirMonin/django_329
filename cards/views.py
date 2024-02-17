db3f3af7931e534e92d493ee5b345990970dd9f0
v <v@mail.ru>
2024-02-17 17:08:12 +0600

    lesson_55: Новая структура маршрутов

--- cards/views.py
+++ cards/views.py
@@ -1,15 +1,45 @@
+"""
+get_all_cards - возвращает все карточки для представления в каталоге
+get_categories - возвращает все категории для представления в каталоге
+get_cards_by_category - возвращает карточки по категории для представления в каталоге
+get_cards_by_tag - возвращает карточки по тегу для представления в каталоге
+get_detail_card_by_id - возвращает детальную информацию по карточке для представления
+
+"""
+
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Привет, мир!")
 
-def catalog(request):
-    return HttpResponse("Каталог карточек")
-
-
-def get_card_by_id(request, card_id):
-    return HttpResponse(f"Карточка {card_id}")
+def get_all_cards(request):
+    """
+    Возвращает все карточки для представления в каталоге
+    """
+    return HttpResponse('All cards')
 
 
-def get_category_by_name(request, slug):
-    return HttpResponse(f"Категория {slug}")
+def get_categories(request):
+    """
+    Возвращает все категории для представления в каталоге
+    """
+    return HttpResponse('All categories')
+
+
+def get_cards_by_category(request, slug):
+    """
+    Возвращает карточки по категории для представления в каталоге
+    """
+    return HttpResponse(f'Cards by category {slug}')
+
+
+def get_cards_by_tag(request, slug):
+    """
+    Возвращает карточки по тегу для представления в каталоге
+    """
+    return HttpResponse(f'Cards by tag {slug}')
+
+
+def get_detail_card_by_id(request, card_id):
+    """
+    Возвращает детальную информацию по карточке для представления
+    """
+    return HttpResponse(f'Detail card by id {card_id}')

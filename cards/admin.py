cf4c6890ba81f8e1803fe54401dbbcc608260a3f
v <v@mail.ru>
2024-03-23 17:35:37 +0500

    lesson_61: админка. кастомные поля в таблице @admin.display()

--- cards/admin.py
+++ cards/admin.py
@@ -2,6 +2,8 @@
 python manage.py createsuperuser - создание суперпользователя
 Есть 2 варианта регистрации
 admin.site.register() или @admin.register()
+@admin.display() - декоратор для отображения дополнительных полей
+параметры декоратора: description - название поля, ordering - сортировка (реальное поле из модели)
 """
 
 from django.contrib import admin
@@ -11,28 +13,42 @@ from .models import Card, Category, Tag, CardTags
 
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    list_display = ('get_questions', 'upload_date', 'category_name', 'tags_list')
-    list_display_links = ('get_questions', 'category_name')
+    list_display = ('get_questions', 'upload_date', 'category_name', 'tags_list', 'brief_info')
+    list_display_links = ('get_questions',)
     list_filter = ('category_id',)
     search_fields = ('question', 'category_id__name', 'answer', 'tags__name')
     ordering = ('-upload_date', 'question')
+    list_per_page = 20
+
+    # list_editable = ('category_name',) # Редактируемое поле
     # Добавляем метод для отображения названия категории
+    @admin.display(description="Категория", ordering='category_id__name')
     def category_name(self, obj):
         return obj.category_id.name
-    category_name.short_description = 'Категория'  # Название колонки в админке
 
     # Дополнительный метод для отображения списка тегов
+    @admin.display(description="Теги", ordering='tags__name')
     def tags_list(self, obj):
         return " | ".join([tag.name for tag in obj.tags.all()])
-    tags_list.short_description = 'Теги'
 
+    # Определение метода для отображения краткой информации о карточке
+    # ordering по полю answer, так как точного поля для сортировки по краткому описанию нет
+    @admin.display(description="Краткое описание", ordering='answer')
+    def brief_info(self, card):
+        # Определяем длину ответа
+        length = len(card.answer)
+        # Проверяем наличие кода
+        has_code = 'Да' if '```' in card.answer else 'Нет'
+        return f"Длина ответа: {length}, Код: {has_code}"
 
     # Дополнительный метод для отображения вопросов
     def get_questions(self, obj):
         row_question = obj.question
         result_question = row_question.replace('##', '').replace('`', '').replace('**', '').replace('*', '')
         return result_question
+
     get_questions.short_description = 'Вопрос'
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
